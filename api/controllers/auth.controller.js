cdfa4ed0fee9cc171b2bcba2473bbeecf64f3d5f
hans kang <hans.kkang@gmail.com>
2025-02-11 14:19:22 -0500

    created signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -43,7 +45,20 @@ export const signin = async (req, res, next) => {
     if (!validPassword) {
       return next(errorHandler(400, 'Invalid password'));
     }
+    
+    const token = jwt.sign(
+      { id: validUser._id},process.env.JWT_SECRET
+    );
 
+    
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
+      .json(rest);
 
   }catch(error){
     next(error);
