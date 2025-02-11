713096e277631244ff659a9e026f1ac564a38d08
hans kang <hans.kkang@gmail.com>
2025-02-11 01:33:39 -0500

    dotenv installed

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+
+dotenv.config();
+
+
+mongoose
+.connect(process.env.MONGO)
+.then (
+    () => {console.log('mongo connected');
+
+    }
+);
 
 const app = express();
 
@@ -6,3 +20,4 @@ app.listen(3000, () => {
 
     console.log('servers running on 3000');
 });
+
