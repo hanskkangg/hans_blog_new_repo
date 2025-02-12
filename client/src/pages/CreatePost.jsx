8066794703442f4794eb1ab8862c1a679c44d776
hans kang <hans.kkang@gmail.com>
2025-02-12 02:38:59 -0500

    complete upload image on create post

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -3,6 +3,17 @@ import ReactQuill, { Quill } from 'react-quill'; // ✅ Import Quill
 import 'react-quill/dist/quill.snow.css';
 import { forwardRef, useRef, useState } from 'react';
 import ImageUploader from 'quill-image-uploader';
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+
+import { app } from '../firebase';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 
 // ✅ Register the image uploader module
 Quill.register('modules/imageUploader', ImageUploader);
@@ -42,8 +53,57 @@ const CustomQuill = forwardRef((props, ref) => (
 ));
 
 export default function CreatePost() {
+  
+  const [file, setFile] = useState(null);
+
+  const [imageUploadProgress, setImageUploadProgress] = useState(null);
+
+  const [imageUploadError, setImageUploadError] = useState(null);
+  
+  const [formData, setFormData] = useState({});
+
   const quillRef = useRef(null);
   const [content, setContent] = useState(''); // ✅ Add content state
+
+
+  
+  const handleUpdloadImage = async () => {
+    try {
+      if (!file) {
+        setImageUploadError('Please select an image');
+        return;
+      }
+      setImageUploadError(null);
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + '-' + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setImageUploadProgress(progress.toFixed(0));
+        },
+        (error) => {
+          setImageUploadError('Image upload failed');
+          setImageUploadProgress(null);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setImageUploadProgress(null);
+            setImageUploadError(null);
+            setFormData({ ...formData, image: downloadURL });
+          });
+        }
+      );
+
+    } catch (error) {
+      setImageUploadError('Image upload failed');
+      setImageUploadProgress(null);
+      console.log(error);
+    }
+  };
 
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
@@ -61,12 +121,35 @@ export default function CreatePost() {
         </div>
 
         <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
-          <FileInput type='file' accept='image/*' />
-          <Button type='button' gradientDuoTone='purpleToBlue' size='sm' outline>
-            Upload Image
-          </Button>
+          <FileInput type='file' accept='image/*' 
+            onChange={(e) => setFile(e.target.files[0])} />
+          <Button type='button' gradientDuoTone='purpleToBlue' size='sm' outline 
+            onClick={handleUpdloadImage}
+            disabled={imageUploadProgress}>
+           
+           {imageUploadProgress ? (
+              <div className='w-16 h-16'>
+                <CircularProgressbar
+                  value={imageUploadProgress}
+                  text={`${imageUploadProgress || 0}%`}
+                />
+              </div>
+            ) : (
+              'Upload Image'
+            )}         
+              
+              </Button>
         </div>
 
+        {imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>}
+        {formData.image && (
+          <img
+            src={formData.image}
+            alt='upload'
+            className='w-full h-72 object-cover'
+          />
+        )}
+        
         <CustomQuill
           ref={quillRef}
           theme='snow'
