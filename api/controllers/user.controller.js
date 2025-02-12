fd819b9fefcc7e99bffe1a707ce8f62eef1a37f9
hans kang <hans.kkang@gmail.com>
2025-02-11 23:22:15 -0500

    update user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,35 @@
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import User from '../models/user.model.js';
+
+// List of prohibited words
+const prohibitedWords = [
+    'nigger', 'fuck', 'shit', 'bitch', 'asshole', 'racist', 'niger', 'nig3r', 'nigg3r',
+    'chink', 'ching', 'bastard', 'damn', 'crap', 'dick', 'pussy', 'cunt', 'twat',
+    'bollocks', 'prick', 'wanker', 'douche', 'motherfucker', 'idiot', 'moron', 
+    'hoe',
+  
+    // Slurs & Hate Speech
+    'nigga', 'faggot', 'homo', 'tranny',
+  
+    // Derogatory Terms Related to Disabilities
+    'retard', 'cripple', 'spaz', 'mongoloid',
+  
+    // Sexual Content
+    'whore', 'slut', 'cum', 'jizz', 'fap', 'porn', 'dildo', 'nude', 'boobs', 
+    'tits', 'vagina', 'penis', 'orgy', 'rape', 'molest', 'incest', 'blowjob',
+  
+    // Violent Terms
+    'kill', 'murder', 'terrorist', 'bomb', 'explode', 'genocide', 'massacre', 
+    'lynch', 'assassinate', 'stab', 'behead', 'shoot', 'abuse', 'pedophile',
+  
+    // Racist & Extremist References
+    'hitler', 'nazi','whitepower', 'supremacy', 'zionist', 'jihadi',
+  
+    // Self-Harm & Suicide References
+    'suicide', 'selfharm', 'cutting', 'die', 'hang', 'overdose', 'depress', 'killself'];
+
+
 export const test = (req,res) =>  {
 
     res.json({message:'working'});
@@ -6,4 +38,61 @@ export const test = (req,res) =>  {
 
 export const updateUser = async (req, res, next) => {
 
+
+    if (req.user.id !== req.params.userId) {
+        return next(errorHandler(403, 'You are not allowed to update this user'));
+      }
+
+      
+  if (req.body.password) {
+    if (req.body.password.length < 6) {
+      return next(errorHandler(400, 'Password must be at least 6 characters'));
+    }
+    req.body.password = bcryptjs.hashSync(req.body.password, 10);
+  }
+
+  if (req.body.username) {
+    if (req.body.username.length < 4 || req.body.username.length > 20) {
+      return next(
+        errorHandler(400, 'Username must be between 4 and 20 characters')
+      );
+    }
 }
+if (req.body.username.includes(' ')) {
+    return next(errorHandler(400, 'Username cannot contain spaces'));
+  }
+  if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+    return next(
+      errorHandler(400, 'Username can only contain letters and numbers')
+    );  
+  }
+
+   // Check for prohibited words
+   for (const badWord of prohibitedWords) {
+    if (username.includes(badWord)) {
+      return next(errorHandler(400, 'Username contains inappropriate language'));
+    }
+  }
+
+
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          profilePicture: req.body.profilePicture,
+          password: req.body.password,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
