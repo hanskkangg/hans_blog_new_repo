696a67d4b96a0913896732f71ed9dfff0f0c6d9b
hans kang <hans.kkang@gmail.com>
2025-02-12 15:18:41 -0500

    reinstalled tailwind

--- client/src/components/DashPost.jsx
+++ client/src/components/DashPost.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
 
-const DashPost = () => {
-  return (
-    <div>DashPost</div>
-  )
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
+export default function DashPosts() {
+    
+  const { currentUser } = useSelector((state) => state.user);
+  const [userPosts, setUserPosts] = useState([]);
+
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        const data = await res.json();
+        if (res.ok) {
+          setUserPosts(data.posts);
+          if (data.posts.length < 9) {
+            setShowMore(false);
+          }
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    if (currentUser.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser._id]);
+
+
+    return
 }
-
-export default DashPost
