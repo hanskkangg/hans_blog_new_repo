2edcff90ed9a818850421b3446aa6eff1dc76b04
hans kang <hans.kkang@gmail.com>
2025-02-12 00:21:47 -0500

    added user delete api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -60,10 +60,6 @@ if (req.body.username) {
     }
   }
 }
-
-
-
-
   try {
     const updatedUser = await User.findByIdAndUpdate(
       req.params.userId,
@@ -79,6 +75,19 @@ if (req.body.username) {
     );
     const { password, ...rest } = updatedUser._doc;
     res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+export const deleteUser = async (req, res, next) => {
+  if (!req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to delete this user'));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json('User has been deleted');
   } catch (error) {
     next(error);
   }
