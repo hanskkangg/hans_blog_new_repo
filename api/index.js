4947080b592118a34cef36a3b05504879795b3ff
hans kang <hans.kkang@gmail.com>
2025-02-15 12:26:44 -0500

    added installation

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import authRoutes from './routes/auth.route.js'
 import commentRoutes from './routes/comment.route.js';
 import postRoutes from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
-
+import path from 'path';
 
 dotenv.config();
 
@@ -18,6 +18,8 @@ mongoose
 
     }
 );
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -46,6 +48,11 @@ app.use('/api/post', postRoutes);
 
 app.use('/api/comment', commentRoutes);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
