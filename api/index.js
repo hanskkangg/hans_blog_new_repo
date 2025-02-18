a2175e74a1c8ed16c0f187b089e00817e29faac9
hans kang <hans.kkang@gmail.com>
2025-02-18 11:14:31 -0500

    updated port

--- api/index.js
+++ api/index.js
@@ -26,9 +26,11 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
-});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
