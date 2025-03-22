69699ccab0bc2b5fc20483f1bba2b73345cd0f01
hans kang <hans.kkang@gmail.com>
2025-03-21 22:13:26 -0400

    domain updated

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -8,7 +8,7 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "personalblog-ca39a.firebaseapp.com",
+  authDomain: "hans-blogs.onrender.com", // Updated to match your hosted domain
   projectId: "personalblog-ca39a",
   storageBucket: "personalblog-ca39a.appspot.com", // Fixed storage bucket URL
   messagingSenderId: "568011967196",
