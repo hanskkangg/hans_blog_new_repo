b771881e21ae02789447025a7a9c0810ee35b9bd
hans kang <hans.kkang@gmail.com>
2025-03-21 21:44:22 -0400

    firebase + mongodb updated

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -10,12 +10,12 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "personalblog-ca39a.firebaseapp.com",
   projectId: "personalblog-ca39a",
-  storageBucket: "personalblog-ca39a.firebasestorage.app",
+  storageBucket: "personalblog-ca39a.appspot.com", // Fixed storage bucket URL
   messagingSenderId: "568011967196",
   appId: "1:568011967196:web:285aab875e7be8e0204219",
-  measurementId: "G-198440CGJ2"
+  measurementId: "G-198440CGJ2",
+  clientId: "86754690739-10m8d0imng6qkiblakl4gsh8rjq5gntl.apps.googleusercontent.com" // Added OAuth client ID
 };
-
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
