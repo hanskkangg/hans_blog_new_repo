4846dc7f276797f7fab70a41d0e579987e811ab9
hans kang <hans.kkang@gmail.com>
2025-03-30 23:52:48 -0400

    Allows up to 10MB of JSON data

--- api/index.js
+++ api/index.js
@@ -20,7 +20,8 @@ mongoose.connect(process.env.MONGO)
   .catch((err) => console.error('MongoDB Connection Error:', err));
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: '10mb' })); 
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 
 // API Logger
